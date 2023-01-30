1e96c7027edb2a5bbd046dc77d8c7f6ca41661d1
fer20305 <fernandez20305@gmail.com>
2023-01-30 00:41:44 -0300

    finalizacion de estilos y creacion de formulario

--- js/funciones.js
+++ js/funciones.js
@@ -16,3 +16,43 @@ function barra() {
     }
   }
 }
+
+function openForm() {
+  document.getElementById("miformulario").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("miformulario").style.display = "none";
+}
+function enviarformulario(){
+      document.formulario.submit();
+      closeForm();
+ }
+function experiencia(){
+  barra();
+  document.getElementById("exp").style.display="block";
+  document.getElementById("est").style.display="none";
+  document.getElementById("hab").style.display="none";
+  document.getElementById("info").style.display="none"; 
+}
+function estudios(){
+  barra();
+  document.getElementById("exp").style.display="none";
+  document.getElementById("est").style.display="block";
+  document.getElementById("hab").style.display="none";
+  document.getElementById("info").style.display="none"; 
+}
+function habilidades(){
+  barra();
+  document.getElementById("exp").style.display="none";
+  document.getElementById("est").style.display="none";
+  document.getElementById("hab").style.display="block";
+  document.getElementById("info").style.display="none"; 
+}
+function informacion(){
+  barra();
+  document.getElementById("exp").style.display="none";
+  document.getElementById("est").style.display="none";
+  document.getElementById("hab").style.display="none";
+  document.getElementById("info").style.display="block"; 
+}
